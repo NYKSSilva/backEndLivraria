170286a6a06af5b9d52131f7ec9f1eced5f03611
NYKSSilva <nicollysantossilva0508@gmail.com>
2025-11-04 11:01:57 -0300

    Livraria

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
-
+// ============================
+//  Dependências
+// ============================
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import usuarioRoutes from "./routes/usuario.route.js"
-
+import usuarioRoutes from "./routes/usuario.routes.js"
+import livrosRoutes from "./routes/livros.routes.js"
+import avaliacoesRoutes from "./routes/avaliacoes.routes.js"
+// ============================
+//  Configuração do servidor
+// ============================
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,6 +19,11 @@ app.get("/", (req,res)=>{
 })
 
 app.use("/usuarios", usuarioRoutes)
+app.use("/livros", livrosRoutes)
+app.use("/avaliacoes", avaliacoesRoutes)
 
+// ============================
+//  Inicia o servidor
+// ============================
 const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
